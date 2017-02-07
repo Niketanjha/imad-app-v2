10676994f488b040eed440b35a1b6ea93db6f8ab
Niketan jha <niketanjha@gmail.com>
2017-02-08 00:24:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/article-one',function(req,res){
-    res.send('Article one requested and will be served here');
-}); 
-
 app.get('/article-two',function(req,res){
     res.send('Article two requested and will be served here');
 }); 
