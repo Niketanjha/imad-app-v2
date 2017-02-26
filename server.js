98178381276c4c9614e294f2d7e4b2c4dba0c23b
Niketan jha <niketanjha@gmail.com>
2017-02-26 17:25:49 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.get('/', function (req, res) {
 });
 
 var counter=0;
-app.get('/counter',function(req,res){
+app.get('/counter',function (req,res){
     counter=counter+1;
-    res.send(counter.tostring());
+    res.send(counter.toString());
 });
 
 app.get('/article-one', function (req, res) {
