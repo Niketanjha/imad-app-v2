d1f39d3d83351e1a7689428c1981462fd40a1965
Niketan jha <niketanjha@gmail.com>
2017-03-31 23:55:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var crypto= require('crypto');
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var counter=0;
-app.get('/counter',function (req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
-
-//this function is for converting given input to hash function
-function hash (input,salt){
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
-}
-app.get('/hash/:input/',function(req,res){
-    var hashedString = hash(req.params.input,'this-is-some-random-string');
-    
-    
 });
 
 app.get('/article-one', function (req, res) {
@@ -41,6 +23,22 @@ app.get('/article-three',function(req,res){
     res.send('Article three requested and will be served here');
 }); 
 
+function hash (input,salt){
+	var hashed= crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+ }
+
+app.get('/hash/:input',function(req,res){
+	var hashedString= hash(req.params.input,'this-is-some-random-string');
+	res.send(hashedString);
+});
+
+app.get('/ui/cover.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'cover.jpg'));
+});
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -49,12 +47,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/cover.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'cover.jpg'));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
