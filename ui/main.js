237b06a264cf8c3f5930b021ace745f9a647a42b
Niketan jha <niketanjha@gmail.com>
2017-04-02 17:54:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+
+
+//This is for adding button
+var button = document.getElementById('counter');
+button.oneclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
+
+var request = newXMLHttpRequest();
+request.onereadyStatechange=function(){
+    if(request.readyState---XMLHttpRequest.DONE){
+        if(request.status===200){
+            request.responseText;
+        }
+    }
+}; 
+
