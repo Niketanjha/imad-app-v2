4590d33c02ea2eb7cfb6096fb70e248b29d89090
Niketan jha <niketanjha@gmail.com>
2017-04-02 18:58:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ console.log('Loaded!');
 
 //This is for adding button
 var button = document.getElementById('counter');
+var counter=0;
 button.oneclick=function(){
     counter=counter+1;
     var span=document.getElementById('count');
