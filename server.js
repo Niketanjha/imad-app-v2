108a74f89bea723a0a04866aead0a1369feea701
Niketan jha <niketanjha@gmail.com>
2017-04-02 17:53:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,25 +5,6 @@ var crypto = require('crypto');
 var app = express();
 app.use(morgan('combined'));
 
-//This is for adding button
-var button = document.getElementById('counter');
-button.oneclick=function(){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-};
-
-var request = newXMLHttpRequest();
-request.onereadyStatechange=function(){
-    if(request.readyState---XMLHttpRequest.DONE){
-        if(request.status===200){
-            request.responseText
-        }
-    }
-}; 
-
-
-
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
